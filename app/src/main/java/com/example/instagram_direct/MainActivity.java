8b12c22a54fe5e86bc49d3d574e7248058fe2b74
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-10-02 12:21:51 +0530

    Change the direct text color to white and added a new functionality that if messages are greater than 9 it will be shown as 9+

--- app/src/main/java/com/example/instagram_direct/MainActivity.java
+++ app/src/main/java/com/example/instagram_direct/MainActivity.java
@@ -10,14 +10,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txt,name;
+    TextView txt, name;
     Integer tap;
     CardView cardView;
 
     Integer count;
 
     public void cameraclick(View view) {
-        Toast.makeText(this, "camera icon click", Toast.LENGTH_SHORT).show( );
+        Toast.makeText(this, "camera icon click", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -25,49 +25,51 @@ public class MainActivity extends AppCompatActivity {
         if (tap <= count) {
             cardView.setVisibility(View.VISIBLE);
             tap = tap + 1;
-            txt.setText(tap.toString( ));
+            if (tap > 9)
+                txt.setText("9+");
+            else
+                txt.setText(tap.toString());
         }
     }
-        public void decrease (View view){
-            if (tap > 0) {
-                tap = tap - 1;
-                txt.setText(tap.toString( ));
-            }
-            if (tap == 0) {
-                cardView.setVisibility(View.INVISIBLE);
 
-            }
+    public void decrease(View view) {
+        if (tap > 0) {
+            tap = tap - 1;
+            txt.setText(tap.toString());
         }
-
-
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            txt = findViewById(R.id.textView);
-            tap = 0;
-            cardView = findViewById(R.id.card);
+        if (tap == 0) {
             cardView.setVisibility(View.INVISIBLE);
-            count=99;
-
-          /**
-            //to set image at left corner
-            imageView=findViewById(R.id.imageView3);
-            imageView.setImageResource(R.drawable.ic_baseline_camera_alt_24);
-
-            //to set title text
-            name=findViewById(R.id.name);
-            name.setText("INSTAGRAM");
-
-            //to set color for notification badge
-            cardView.setCardBackgroundColor();
-           //to set count limit for notification
-           count=99;
-
-            **/
-
-
 
         }
     }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+        tap = 0;
+        cardView = findViewById(R.id.card);
+        cardView.setVisibility(View.INVISIBLE);
+        count = 99;
+
+        /**
+         //to set image at left corner
+         imageView=findViewById(R.id.imageView3);
+         imageView.setImageResource(R.drawable.ic_baseline_camera_alt_24);
+
+         //to set title text
+         name=findViewById(R.id.name);
+         name.setText("INSTAGRAM");
+
+         //to set color for notification badge
+         cardView.setCardBackgroundColor();
+         //to set count limit for notification
+         count=99;
+         **/
+
+
+    }
+}
 
